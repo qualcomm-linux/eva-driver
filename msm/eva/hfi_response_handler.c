e8c80c83aba715366cbb884d19defc4d6ada06fa
VIVEK VARDHAN JOSHI <quic_vivekvar@quicinc.com>
2024-12-11 12:38:24 +0530

    msm: eva: OOB read fixes in FW response handling function
    Potential OOB read issues in handling FW response
    (dump_notify, process_cvp_msg functions)(5428588).
    
    Change-Id: I17219983a922750cf2f0319aec3f7c671a86e054
    Signed-off-by: VIVEK VARDHAN JOSHI <quic_vivekvar@quicinc.com>

--- msm/eva/hfi_response_handler.c
+++ msm/eva/hfi_response_handler.c
@@ -520,10 +520,15 @@ static int hfi_process_session_dump_notify(u32 device_id,
 	if (!pkt) {
 		dprintk(CVP_ERR, "%s: invalid param\n", __func__);
 		return -EINVAL;
-	} else if (pkt->header.size > sizeof(struct cvp_hfi_dumpmsg_session_hdr)) {
-		dprintk(CVP_ERR, "%s: bad_pkt_size %d\n", __func__, pkt->header.size);
-		return -E2BIG;
+	} else if (pkt->header.size != sizeof(struct cvp_hfi_dumpmsg_session_hdr)) {
+		dprintk(CVP_ERR, "%s: bad_pkt_size %d, expected pkt_size %d\n",
+			__func__, pkt->header.size, sizeof(struct cvp_hfi_dumpmsg_session_hdr));
+		if (pkt->header.size > sizeof(struct cvp_hfi_dumpmsg_session_hdr))
+			return -E2BIG;
+		else
+			return -EINVAL;
 	}
+
 	session_id = get_msg_session_id(pkt);
 	core = cvp_driver->cvp_core;
 	inst = cvp_get_inst_from_id(core, session_id);
@@ -570,7 +575,12 @@ static int hfi_process_session_cvp_msg(u32 device_id,
 	} else if (pkt->header.size > MAX_HFI_PKT_SIZE * sizeof(unsigned int)) {
 		dprintk(CVP_ERR, "%s: bad_pkt_size %d\n", __func__, pkt->header.size);
 		return -E2BIG;
+	} else if (pkt->header.size < get_msg_size(pkt)) {
+		dprintk(CVP_ERR, "%s: bad_pkt_size %d, expected pkt size %d\n",
+			__func__, pkt->header.size, get_msg_size(pkt));
+		return -EINVAL;
 	}
+
 	session_id = get_msg_session_id(pkt);
 	core = cvp_driver->cvp_core;
 	inst = cvp_get_inst_from_id(core, session_id);
