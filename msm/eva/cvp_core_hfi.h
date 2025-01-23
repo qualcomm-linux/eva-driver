5915fe63a2ff9856bdc23d4d8148bd9f8abc6b1a
Pranay Varma Kopanati <quic_pkopanat@quicinc.com>
2025-01-23 13:01:03 +0530

    msm: eva: [set-1] Porting changes from sun target
    Below changes:
    - Error handling when queues init fails.
    - Excessive warning log fix in validating inst.
    - Added Timeline marker print.
    
    Change-Id: I29cdc8a9442b7f8650c407f3648032c97ab9537d
    Signed-off-by: Pranay Varma Kopanati <quic_pkopanat@quicinc.com>
    Signed-off-by: George Shen <quic_sqiao@quicinc.com>

--- msm/eva/cvp_core_hfi.h
+++ msm/eva/cvp_core_hfi.h
@@ -1,7 +1,7 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 /*
  * Copyright (c) 2018-2021, The Linux Foundation. All rights reserved.
- * Copyright (c) 2023-2024 Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) 2023-2025 Qualcomm Innovation Center, Inc. All rights reserved.
  */
 
 #ifndef __H_CVP_CORE_HFI_H__
@@ -340,6 +340,10 @@ struct msm_cvp_inst *cvp_get_inst_from_id(struct msm_cvp_core *core,
 		ktid = (cmd_hdr->header.client_data.kdata  & (FENCE_BIT - 1)); \
 		trace_tracing_eva_frame_from_sw(aon_cycles, tag, sess_id, \
 			stream_id, pkt_id, t_id, ktid); \
+		dprintk(CVP_INFO, \
+			"tracing_eva_frame_from_sw: AON_TIMESTAMP: %llu %s session_id = 0x%x "\
+			"stream_id = 0x%x packet_id = 0x%x transaction_id = 0x%x ktid = 0x%x\n",\
+			aon_cycles, tag, sess_id, stream_id, pkt_id, t_id, ktid); \
 	} \
 })
 
@@ -361,6 +365,10 @@ struct msm_cvp_inst *cvp_get_inst_from_id(struct msm_cvp_core *core,
 		ktid = (msg_hdr->header.client_data.kdata  & (FENCE_BIT - 1)); \
 		trace_tracing_eva_frame_from_sw(aon_cycles, tag, session_id, \
 			stream_id, pkt_id, t_id, ktid); \
+		dprintk(CVP_INFO,\
+			"tracing_eva_frame_from_sw: AON_TIMESTAMP: %llu %s session_id = 0x%x "\
+			"stream_id = 0x%x packet_id = 0x%x transaction_id = 0x%x ktid = 0x%x\n",\
+			aon_cycles, tag, session_id, stream_id, pkt_id, t_id, ktid); \
 	} \
 })
 
