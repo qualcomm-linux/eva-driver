be36effeef368e44336e5a2cda804910baf7809b
Arunteja Reddy Gopireddy <garuntej@qti.qualcomm.com>
2026-02-20 09:39:26 +0530

    msm: eva: Enabling Pebble EVA driver
    Added bzl, platform driver changes and updated QOS settings.
    
    Change-Id: If0d4fd1247c737976948136646e31137bde7b66d
    Signed-off-by: Arunteja Reddy Gopireddy <garuntej@qti.qualcomm.com>

--- msm/eva/msm_cvp_platform.c
+++ msm/eva/msm_cvp_platform.c
@@ -579,6 +579,69 @@ static struct msm_cvp_common_data sm8975_common_data[] = {
 	}
 };
 
+static struct msm_cvp_common_data sm8950_common_data[] = {
+	{
+		.key = "qcom,pm-qos-latency-us",
+		.value = 50,
+	},
+	{
+		.key = "qcom,sw-power-collapse",
+#ifdef USE_PRESIL
+		.value = 0,
+#else
+		.value = 1,
+#endif
+	},
+	{
+		.key = "qcom,domain-attr-non-fatal-faults",
+		.value = 0,
+	},
+	{
+		.key = "qcom,max-secure-instances",
+		.value = 2,
+	},
+	{
+		.key = "qcom,max-supported-instances",
+		.value = 32,
+	},
+	{
+		.key = "qcom,max-ssr-allowed",
+		.value = 1,
+	},
+	{
+		.key = "qcom,power-collapse-delay",
+		.value = 3000,
+	},
+	{
+		.key = "qcom,hw-resp-timeout",
+#ifdef USE_PRESIL
+		.value = 60000,
+#else
+		.value = 2000,
+#endif
+	},
+	{
+		.key = "qcom,dsp-resp-timeout",
+		.value = 1000,
+	},
+	{
+		.key = "qcom,debug-timeout",
+		.value = 0,
+	},
+	{
+		.key = "qcom,dsp-enabled",
+		.value = 1,
+	},
+	{
+		.key = "qcom,qos_noc_urgency_low_a_bitmask",
+		.value = 0x30,
+	},
+	{
+		.key = "qcom,qos_noc_urgency_low_b_bitmask",
+		.value = 0x3,
+	}
+};
+
 /* Default UBWC config for LPDDR5 */
 static struct msm_cvp_ubwc_config_data kona_ubwc_data[] = {
 	UBWC_CONFIG(1, 1, 1, 0, 0, 0, 8, 32, 16, 0, 0),
@@ -744,6 +807,20 @@ static struct msm_cvp_platform_data sm8975_data = {
 	.hal_version = HAWI_HAL_VER,
 };
 
+static struct msm_cvp_platform_data sm8950_data = {
+	.common_data = sm8950_common_data,
+	.common_data_length = ARRAY_SIZE(sm8950_common_data),
+	.sku_version = 0,
+	.vpu_ver = VPU_VERSION_5,
+	.ubwc_config = kona_ubwc_data,	/*Reuse Kona setting*/
+	.noc_qos = &pakala_noc_qos,
+	.vm_id = 1,
+	.cvp_hfi = cvp_hfi_defs_v2,
+	.cvp_hfi_msg = cvp_hfi_msg_defs_v2,
+	.hfi_ver = 2,
+	.hal_version = HAWI_HAL_VER,
+};
+
 static const struct of_device_id msm_cvp_dt_match[] = {
 	{
 		.compatible = "qcom,waipio-cvp",
@@ -780,6 +857,10 @@ static const struct of_device_id msm_cvp_dt_match[] = {
 	{
 		.compatible = "qcom,art-cvp",
 		.data = &sm8975_data,
+	},
+	{
+		.compatible = "qcom,pebble-cvp",
+		.data = &sm8950_data,
 	},
 	{},
 };
